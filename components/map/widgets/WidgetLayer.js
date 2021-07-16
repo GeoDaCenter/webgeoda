9580face89271896dc81df9d342898696c66696c
Eric <emjyoon@gmail.com>
2021-07-16 15:34:43 -0700

    Migrate widget styles to scss

--- components/map/widgets/WidgetLayer.js
+++ components/map/widgets/WidgetLayer.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import styles from './Widgets.module.css';
+import styles from './Widgets.module.scss';
 import Widget from "./Widget";
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
